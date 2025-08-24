55f6272f534531b2b833db61a7395aad35ea9fae
abdelkader-belhaj <abdelkader.belhaj@esprit.tn>
2025-08-25 00:40:37 +0100

    Ajout Health Check et wwwroot pour Azure

---
+++ src/Controller/HealthControllerPhpController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class HealthControllerPhpController extends AbstractController
+{
+    #[Route('/health', name: 'health_check')]
+public function check(): Response
+{
+    return new Response('OK', 200);
+}
+}
